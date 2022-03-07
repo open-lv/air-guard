e4596af54861e4ea2d20c5ccc478ce774f3cd701
RonnyLV <hamste126@gmail.com>
2022-03-07 03:13:11 +0200

    uPy: Added possibility to save configuration to JSON file

--- firmware/micropython/config.py
+++ firmware/micropython/config.py
@@ -30,22 +30,46 @@ class SargsConfig:
     # Currently implemented: ThingspeakMQTTClient, requires MQTT_TS_CHANNEL
     MQTT_CLASS = "ThingspeakMQTTClient"
 
-    _json_mapping = [
+    _json_mapping = {
         # JSON field, Class attribute, type
-        ("wifiEnabled", "WIFI_ENABLED", bool),
-        ("wifiSsid", "WIFI_SSID", str),
-        ("wifiPassword", "WIFI_PASSWORD", str),
-        ("captivePortalEnabled", "CAPTIVE_PORTAL_ENABLED", bool),
-        ("mqttClientId", "MQTT_CLIENT_ID", str),
-        ("mqttUsername", "MQTT_USERNAME", str),
-        ("mqttPassword", "MQTT_PASSWORD", str),
-        ("mqttPublishIntervalS", "MQTT_PUBLISH_INTERVAL_S", int),
-        ("mqttTsChannel", "MQTT_TS_CHANNEL", str),
-        ("mqttClass", "MQTT_CLASS", str),
-    ]
+        "WIFI_ENABLED": ("wifiEnabled", bool),
+        "WIFI_SSID": ("wifiSsid", str),
+        "WIFI_PASSWORD": ("wifiPassword", str),
+        "CAPTIVE_PORTAL_ENABLED": ("captivePortalEnabled", bool),
+        "MQTT_CLIENT_ID": ("mqttClientId", str),
+        "MQTT_USERNAME": ("mqttUsername", str),
+        "MQTT_PASSWORD": ("mqttPassword", str),
+        "MQTT_PUBLISH_INTERVAL_S": ("mqttPublishIntervalS", int),
+        "MQTT_TS_CHANNEL": ("mqttTsChannel", str),
+        "MQTT_CLASS": ("mqttClass", str),
+    }
+
+    def __setattr__(self, name, value) -> None:
+        if name not in self._json_mapping:
+            raise Exception('Invalid configuration key "%s"' % name)
+
+        (config_field, config_type) = self._json_mapping[name]
+        if value is not None and not isinstance(value, config_type):
+            raise Exception('Invalid configuration value for "%s", must be of type "%s"' % (name, config_type.__name__))
+
+        super().__setattr__(name, value)
 
     def save(self):
-        pass
+        try:
+            with open("config.json") as config_file:
+                config_json = ujson.load(config_file)
+        except:
+            self.logger.info("config.json doesn't exist")
+            config_json = {}
+
+        for global_name in self._json_mapping:
+            (config_field, config_type) = self._json_mapping[global_name]
+            config_json[config_field] = getattr(self, global_name)
+
+        with open("config.json", "w") as config_file:
+            ujson.dump(config_json, config_file)
+
+        self.logger.info("config.json written successfully!")
 
     def __init__(self):
         try:
@@ -57,12 +81,10 @@ class SargsConfig:
         with open("config.json") as config_file:
             config_json = ujson.load(config_file)
 
-        for json_attr in self._json_mapping:
-            (config_field, global_name, config_type) = json_attr
+        for global_name in self._json_mapping:
+            (config_field, config_type) = self._json_mapping[global_name]
             if config_field not in config_json:
                 continue
-            if not isinstance(config_json[config_field], config_type):
-                raise Exception('Configuration "%s" must be of type "%s"' % (config_field, config_type.__name__))
             setattr(self, global_name, config_json[config_field])
 
 
